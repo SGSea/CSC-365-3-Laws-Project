79558ed1a01ee3882da96246b6489801e45e5bf7
SGSea <samuelgrantcaldwell@gmail.com>
2016-02-25 13:37:12 -0600

    broke it

--- gaurdbot/scripts/wander.py
+++ gaurdbot/scripts/wander.py
@@ -3,10 +3,9 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 import math
-from kobuki_msgs.msg import BumperEvent, CliffEvent
 
 # Every Python Controller needs these lines
-import roslib; roslib.load_manifest('project1')
+# import roslib; roslib.load_manifest('project1')
 
 class Scan_msg:
 
@@ -24,7 +23,7 @@ class Scan_msg:
 		self.ang = {0:0,001:-1.2,10:-1.2,11:-1.2,100:1.5,101:1.0,110:1.0,111:1.2}
 		self.fwd = {0:.25,1:0,10:0,11:0,100:0.1,101:0,110:0,111:0}
 		self.dbgmsg = {0:'Move forward',1:'Veer right',10:'Veer right',11:'Veer right',100:'Veer left',101:'Veer 	left',110:'Veer left',111:'Veer right'}
-	
+
 	def reset_sect(self):
 		'''Resets the below variables before each new scan message is read'''
 		self.sect_1 = 0
@@ -42,7 +41,7 @@ class Scan_msg:
 		entries = len(laserscan.ranges)
 		for entry in range(0,entries):
 			if 0.4 < laserscan.ranges[entry] < 0.75:
-				
+
 				self.sect_1 = 1 if (0 < entry < entries/3) else 0
 				self.sect_2 = 1 if (entries/3 < entry < entries/2) else 0
 				self.sect_3 = 1 if (entries/2 < entry < entries) else 0
@@ -63,12 +62,12 @@ class Scan_msg:
 		self.pub.publish(self.msg)
 
 		self.reset_sect()
-	
+
 	def bumper_event_callback(self, msg):
 		"""Checks to see if the bumper has been pressed, if so the robot will then
 		back up and turn in a random direction."""
 		k = 0
-		if msg.state == BumperEvent.PRESSED:	
+		if msg.state == BumperEvent.PRESSED:
 			for k in range(0, 4999):
 				self.msg.angular.z = 0
 				self.msg.linear.x = -0.1
@@ -114,4 +113,4 @@ if __name__ == "__main__":
 	'''A Scan_msg class object called sub_obj is created and listener
 	function is run'''
 	sub_obj = Scan_msg()
-	listener()	
+	listener()
