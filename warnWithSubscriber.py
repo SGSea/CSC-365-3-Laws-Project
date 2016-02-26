7516bcd46a645e2f8737fe71be19fcfafb035a12
sehrnstein <sehrnstein@gmail.com>
2016-02-26 06:08:03 -0600

    Update warnWithSubscriber.py

--- warnWithSubscriber.py
+++ warnWithSubscriber.py
@@ -4,37 +4,42 @@ import rospy
 from std_msgs.msg import String
 import std_msgs
 import pyttsx
+import unittest
 
 talker = pyttsx.init()
-notice = True
+
+#the following parameters are now initialized in patrol.launch
+#use rosparam set PARAM VALUE to get different results for now
+
+#rospy.set_param('notice', False)
+#rospy.set_param('isShannon',  False)
+#rospy.set_param('isAkash',  False)
+#rospy.set_param('isSam', False)
+#rospy.set_param('isRollin', False)
+#rospy.set_param('isHuman',  False)
+#rospy.set_param('isMoving', False)
 isAuthorized = False
-isShannon = False
-isAkash = False
-isSam = False
-isRollin = False
-isHuman = False
-isMoving = True
 react = "reacting"
 oldReaction = react
+statement = "Initialized"
+
 #The following if loop should be initiated after an intruder is detected
-if notice:
-	if isHuman:
-		if isShannon or isAkash or isSam or isRollin:
+if rospy.get_param('notice'):
+	if rospy.get_param('isHuman'):
+		if rospy.get_param('isShannon') or rospy.get_param('isAkash') or rospy.get_param('isSam') or rospy.get_param('isRollin'):
 			isAuthorized = True
 		if isAuthorized:
 			react = "Hello, Master! Welcome back."
 		if not isAuthorized:
 			react = "You do not have clearance to be here. Leave"
-	if not isHuman:
-		if isMoving:
+	if not rospy.get_param('isHuman'):
+		if rospy.get_param('isMoving'):
 			react = "Leave or be neutralized"
-			#the robot should the attack
-		if not isMoving:
+			#the robot should then attack, with Sam's code
+		if not rospy.get_param('isMoving'):
 			react = "A new object has been detected"
-			#have robot report to Master
+			#have robot report to Master--publisher
 	if react != oldReaction:
-		statement = react
-		print statement
 		oldReaction = react
 		if isAuthorized:
 			statement = "I am resuming my duties"
@@ -43,18 +48,19 @@ if notice:
 
 		talker.say(react)
 		talker.runAndWait()
+	rospy.set_param('notice', False)
 
+#creates a publisher with updates on the node's status
 rospy.init_node('reaction_publisher')
-pubReactions = rospy.Publisher('reactions', String, queue_size=30)
-rate = rospy.Rate(2)
+pubReactions = rospy.Publisher('reactions', String, queue_size=1)
+rate = rospy.Rate(1)
 
 while not rospy.is_shutdown():
-	rospy.loginfo(statement)
-	pubReactions.publish(statement)
-	oldReaction = react
+	rospy.loginfo(react + "..." + statement)
+	pubReactions.publish(react + "..." + statement)
 	rate.sleep()
 
-   # A subscriber for the camera
+   # A subscriber, which will take data from Rollin's publisher when done
 def callback(data):
 	rospy.loginfo("I heard ", data.data)
 	print data.data
